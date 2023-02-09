b518ef5008be920c3abe968a509c6047c2ad1e08
Jefferson-Butler1 <jefferson.butler@icloud.com>
2023-02-09 08:02:49 +0000

    Newtons Method with Synthetic Division -- solves 20 or less degree polynomial for all real roots

--- newtonsMethod.h
+++ newtonsMethod.h
@@ -10,8 +10,7 @@ complex double f(complex double x, complex double coeffs[], int n)
   complex double result = 0;
   int i;
 
-  for (i = 0; i <= n; i++)
-    {
+  for (i = 0; i <= n; i++){
       result = result + pow(x,i) * coeffs[i];
     }
 
@@ -23,20 +22,15 @@ complex double df(complex double x, complex double coeffs[], int degree)
   complex double result = 0;
   int i;
 
-  for (i = 1; i <= degree; i++)
-    {
+  for (i = 1; i <= degree; i++){
       result = result + pow(x,i-1) * i * coeffs[i];
     }
-  if(result == 0){
-    printf("Df = 0\n");
-  }
   return result;
 }
 
 complex double newtonsMethod(complex double x, complex double coeffs[],
 			     int degree, double tolerance)
 {
-  printf("Entering newtonsMethod() %lf + %lfI\n", creal(x), cimag(x));
   complex double x1;
   int i = 0;
   double fVal, dfVal;
@@ -46,9 +40,6 @@ complex double newtonsMethod(complex double x, complex double coeffs[],
       dfVal = df(x, coeffs, degree);
       x = x - (fVal/ dfVal);
       i++;	
-      /* printf("%2d: %lf + %lfI\n", i, creal(x), cimag(x)); */
-      /* printf(" f: %lf, df: %lf\n", cabs(fVal), dfVal); */
-      //sleep(1);
     } while (cabs(fVal) > tolerance);
 
   return x;
--- syntheticDivide.h
+++ syntheticDivide.h
@@ -16,7 +16,15 @@ void syntheticDivide(complex double poly[], int degree,
     for (i = 0; i <= degree; i++){
         temp[i] = poly[i];
     }
-
+    
+    for(i = degree-1; i >= 0; i --){
+	poly[i] += constant * poly[i+1];
+    }
+    remainder[0] = poly[0];
+    for(i = 0; i < degree; i++){
+	result[i] = poly[i+1];
+    }
+/*
     for (i = 0; i < degree; i++){
         result[i] = temp[0] / constant;
         temp[0] = 0;
@@ -28,6 +36,7 @@ void syntheticDivide(complex double poly[], int degree,
 
     result[degree] = temp[0];
     remainder[0] = 0;
+*/
 }
 
 #endif
